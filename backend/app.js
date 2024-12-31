df9103c8183f7ef70ad6d4ff2d96614d028e29d6
Aashay30 <tamrakaraashay30@gmail.com>
2024-12-31 11:53:08 +0530

    logger added

--- backend/app.js
+++ backend/app.js
@@ -1,4 +1,19 @@
-import dotenv from 'dotenv'
-dotenv.config(); // to use environment variables from .env file
-
 import express from 'express'
+import morgan from 'morgan' // a logger - to see all types of request made to server in log
+
+const app = express(); // express server created 
+app.use(morgan('dev'));
+
+// middlewares
+app.use(express.json());
+app.use(express.urlencoded({extended: true}));
+
+// dummy route 
+app.get('/', (req,res) => {
+    res.send('Hello World');
+});
+
+export default app;
+
+// listen in server.js - because we will use socket.io and it require http server
+// so making it separate to use
