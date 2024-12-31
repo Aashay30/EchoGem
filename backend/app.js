bf65e942f8873d54d9d7128edb7179ff5c6ecfbb
Aashay30 <tamrakaraashay30@gmail.com>
2024-12-31 19:38:30 +0530

    user_route_and_module_added

--- backend/app.js
+++ backend/app.js
@@ -1,15 +1,20 @@
 import express from 'express'
 import morgan from 'morgan' // a logger - to see all types of request made to server in log
 
+import userRoutes from './routes/user.routes.js';
+
 import connect from './db/db.js'; // if error shows - use ctrl + shift + p then reload window
 connect();
 
 const app = express(); // express server created 
+
 app.use(morgan('dev'));
 
 // middlewares
 app.use(express.json());
 app.use(express.urlencoded({extended: true}));
+
+app.use('/users', userRoutes);
 
 // dummy route 
 app.get('/', (req,res) => {
